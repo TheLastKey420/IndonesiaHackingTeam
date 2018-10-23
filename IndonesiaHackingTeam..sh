2bf23a465e5d1ceb97a8dcf552e8b4737e950c97
TheLastKey420 <44385454+TheLastKey420@users.noreply.github.com>
2018-10-23 09:33:00 +0800

    Add files via upload

---
+++ IndonesiaHackingTeam..sh
@@ -0,0 +1,286 @@
+#!/bin/bash
+#version 1.0
+
+#                      MAU NGAPAIN TOT?
+#              RECODE?REEDIT?RECOPYRIGHT?COPAS?
+#                       MALU TOLOL!!!
+#                      KAGA BERMORAL!!
+#                     KELUAR KAMPANG!!!
+#              HARGAI KARYA ORANG LAIN GOBLOK!
+
+#07/07/17
+
+clear
+# Variables
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+
+figlet The Last Key | lolcat
+
+echo  "_____________________________________________________________" | lolcat
+echo  "[TYPE      : TOOLS INSTALLER] " |lolcat
+echo  "[VERSION   : V.4] " | lolcat
+echo  "[TOTALS    : 400++ TOOLS] " | lolcat
+echo  "[AUTHOR    : TheLastKey420] " |lolcat
+echo  "[ASSOCIATE : Indonesia Defacer Team] " |lolcat
+echo  "[ASSOCIATE : Hacker NewBie] " |lolcat
+echo  "[ASSOCIATE : Indonesia Hacking Team] " |lolcat
+echo  "[NOTES     : JELAJAHI SETIAP TOOLS DENGAN BIJAK] " | lolcat
+echo  "[THANKS TO : GOD]" | lolcat
+echo  "_____________________________________________________________" | lolcat
+
+sleep 1
+
+###################################################
+# CTRL + C
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo  $green"TOOLS YANG KALIAN INSTALL ADA DI DIRECTORY TOOLS GUA " |lolcat
+echo  $green"Thanks For Using My Tools" |lolcat
+sleep 1
+echo  $green"TheLastKey420 On Your System" |lolcat
+
+echo  $green"See you Again SCRIPTKIDDIES😎 :)..." |lolcat
+echo  $green"BELAJARLAH DARI KESALAHAN YANG SUDAH TERJADI"
+echo  $green"TAPI KALAU KALIAN TAHU ITU SALAH DAN MELAKUKANNYA"
+echo  $green"BERARTI ANDA BODOH,GOBLOK,TOLOL"
+echo  $green"JANGAN SERING-SERING COLI COEG :v"
+figlet TheLastKey420 | lolcat
+sleep 1
+exit
+}
+
+
+lagi=1
+while [ $lagi -lt 6 ];
+do
+
+echo ""                
+echo ""                
+
+echo  "######################################" | lolcat
+echo  "#SILAHKAN TUNGGU DAN LIHAT LIST TOOLS#" | lolcat
+echo  "######################################" | 
+lolcat
+
+echo ""                
+echo  "TOOLS INI CUMAN UNTUK MENGINSTALLNYA SAJA" |lolcat
+echo  "JIKA MAU MENJALANKAN TOOLS YANG KALIAN INSTALL" | lolcat
+echo  "SILAHKAN KETIK 00 LALU KETIK ls" | lolcat
+echo  "DAN MASUK KE TOOLS YANG KALIAN INSTALL" | lolcat
+echo  "JANGAN NANYA LAGI KAMPANG " | lolcat
+echo  "SABAR YAK NUNGGU NYA :v " | lolcat
+echo  "#TTD TheLastKey420 GANS 😎" | lolcat
+echo  "#LastKey MAH BEBAS😎" | lolcat
+echo ""
+echo  "TUNGGU KURANG LEBIH 5 MENIT"
+echo ""  
+echo ""  
+
+echo -e "SelamatMenikmatiHeheheTheLastkeySangPeniruCommad" | lolcat
+echo ""
+echo ""
+echo -e "============================" | lolcat
+echo -e $b "349. Install mitmAP${enda}";
+echo -e "============================" | lolcat
+echo -e $b "350. Install Red-Teamming-Toolkit${enda}";
+echo -e "============================" | lolcat
+echo -e $b "351. Install ssh-mimtm${enda}";
+echo -e "============================" | lolcat
+echo -e $b "352. Install Raccoon${enda}";
+echo -e "============================" | lolcat
+echo -e $b "353. Install CTFR${enda}";
+echo -e "============================" | lolcat
+echo -e $b "354. Install Exploit${enda}";
+echo -e "============================" | lolcat
+echo -e $b "355. Install NoSQLMap${enda}";
+echo -e "============================" | lolcat
+echo -e $b "356. Install Dr0p1t-Framework${enda}";
+echo -e "============================" | lolcat
+echo -e $b "357. Install Subfinder${enda}";
+echo -e "============================" | lolcat
+echo -e $b "358. Install SwiftKeylogger${enda}";
+echo -e "============================" | lolcat
+echo -e $b "359. Install Dockerscan${enda}";
+echo -e "============================" | lolcat
+echo -e $b "388. Install prank${enda}";
+echo -e "============================" | lolcat
+echo -e $b "389. Install Jadwal-Sholat${enda}";
+echo -e "============================" | lolcat
+echo -e $b "390. Install Kubik-Bot${enda}";
+echo -e "============================" | lolcat
+echo -e $b "391. Install Icebreaker${enda}";
+echo -e "============================" | lolcat
+echo -e $b "392. Install Blackarch${enda}";
+echo -e "============================" | lolcat
+echo -e $b "393. Install WEBDAV${enda}";
+echo -e "============================" | lolcat
+echo -e $b "394. Install Weevely${enda}";
+echo -e "============================" | lolcat
+echo -e $b "395. Install Dirsearch.git${enda}";
+echo -e "============================" | lolcat
+echo -e $b "396. Install BluePot.git${enda}";
+echo -e "============================" | lolcat
+echo -e $b "397. Install bot.git${enda}";
+echo -e "============================" | lolcat
+echo -e $b "00. Exit${enda}";
+echo -e "============================" | lolcat
+echo -e "╭─[PILIH NOMORNYA]"
+read -p "╰─root@./TheLastKey=" pil;
+
+
+#Install mitmAP
+
+349) git clone https://github.com/xdavidhu/mitmAP
+
+;;
+
+
+#Install Red-Teamming-Toolkit
+
+350) git clone https://github.com/infosecn1nja/Red-Teaming-Toolkit
+
+;;
+
+#Install ssh-mimtm
+
+351) git clone https://github.com/jtesta/ssh-mitm
+
+;;
+
+#Install Racoon
+
+352) git clone https://github.com/evyatarmeged/Raccoon
+
+;;
+
+#Install CTFR
+
+353) git clone https://github.com/UnaPibaGeek/ctfr
+
+;;
+
+#Install Exploits
+
+354) git clone https://github.com/XiphosResearch/exploits
+
+;;
+
+#Install NoSQLMap
+
+355) git clone https://github.com/codingo/NoSQLMap
+
+;;
+
+#Install Dr0p1t-Framework
+
+356) git clone https://github.com/D4Vinci/Dr0p1t-Framework
+
+;;
+
+#Instal Subfinder
+
+357) git clone https://github.com/subfinder/subfinder
+
+;;
+
+#Install SwiftKeylogger
+
+358) git clone https://github.com/SkrewEverything/Swift-Keylogger
+
+;;
+
+#Install DockerScan
+
+359) git clone https://github.com/cr0hn/dockerscan
+
+;;
+
+#Install Kubik-bot
+
+390) git clone https://github.com/radenvodka/kubik-bot
+
+;;
+
+#Install Icebreaker
+
+391) git clone https://github.com/DanMcInerney/icebreaker
+
+;;
+
+#Install Blackarch
+
+392) git clone https://github.com/BlackArch/blackarch
+
+;;
+
+#Install WEBDAV
+
+393) apt update && apt upgrade
+apt install python2
+pip2 install urllib3 chardet certifi idna requests
+apt install openssl curl
+pkg install libcurl
+mkdir webdav
+cd webdav
+wget https://pastebin.com/raw/HnVyQPtR
+mv HnVyQPtR webdav.py
+chmod 777 webdav.py
+python2 webdav.py
+
+;;
+
+#Install Weevely
+
+394) git clone https://github.com/glides/Weevely
+
+;;
+
+#Install Dirsearch.git
+
+395) git clone https://github.com/maurosoria/dirsearch.git
+
+
+;;
+
+#Install Bluepot.git
+
+396) git clone https://git.kali.org/packages/bluepot.git
+
+;;
+
+#Install Bot.git
+
+397) git clone https://github.com/mrcakil/bot.git
+
+;;
+
+#EXIT
+
+00) echo "AUTHOR: TheLastKey420" | lolcat
+echo "TheLastKey420 ON YOUR SYSTEM" | lolcat
+echo "INDONESIAN HACKING TEAM" | lolcat
+echo "SEMUA TOOLS YANG KALIAN INSTALL ADA DI DIRECTORY TOOLS GUA" | lolcat
+echo "Thank To My Friends=Vhanke-M4TL4Y-Jin-Tomang-BgS4t" | lolcat
+figlet TheLastKey420 | lolcat
+exit
+;;
+
+*) echo "Sorry, Your choices it's not already [T4T]"
+esac
+done
+done
